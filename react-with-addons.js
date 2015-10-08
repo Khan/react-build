70f2bb19318b8166b51de11532460f38ff440e91
Emily Eisenberg <xymostech@gmail.com>
2015-10-08 11:39:58 -0700

    Add `__internalAddons` all the time.
    Summary: Oops, since I made `require("react-addons-{blah}")` look at
    `React.__internalAddons` but that wasn't available in the prod version,
    all of the prod stuff failed badly.
    
    Test plan:
     - In webapp, copy react.prop.js to react.dev.js and see that
       `React.__internalAddons` exists and
       `KAdefine.require("react-addons-create-fragment")` works.
    
    Auditors: alpert

--- react-with-addons.js
+++ react-with-addons.js
@@ -1,12 +1,12 @@
 window.React = require('react/addons');
 window.React.__internalReactMount = require('react/lib/ReactMount');
 window.React.__internalReactDOM = require('react-dom');
+window.React.__internalAddons = window.React.addons;
 
 if ("production" !== process.env.NODE_ENV) {
     var warning =
             require('./node_modules/react/node_modules/fbjs/lib/warning');
 
-    window.React.__internalAddons = window.React.addons;
     window.React.addons = {};
 
     Object.keys(window.React.__internalAddons).forEach(function(addonName) {
